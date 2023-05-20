1f246820d899245c41aa98cd42b177c8b8dd9f58
yousef <ywsfalshry236@gmail.com>
2023-05-20 03:35:09 +0300

    change the input to textarea

--- main.js
+++ main.js
@@ -10,13 +10,13 @@ answer.onreadystatechange= function(){
     if(this.status===200&&this.readyState===4){
         let myAnswer=JSON.parse(this.responseText)
         let corectOrFulse=myAnswer[ "صح او خطا"]
-        
+
         result.onclick=function(){
 
             for(let i = 0; i <= corectOrFulse.length; i++){
                 let allQuiz=corectOrFulse[i].quiz
 
-                if(theQuiz.value.trim()==allQuiz.trim()){
+                if(theQuiz.value.toString().trim()==allQuiz.toString().trim()){
                     if(document.querySelector(".myResult")){
                         document.querySelector(".myResult").remove()
                     }
