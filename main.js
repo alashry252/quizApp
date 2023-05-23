9aa353eefe34a81224d0a746fd9f47db52f7a50b
yousef <ywsfalshry236@gmail.com>
2023-05-24 02:09:04 +0300

    Add Quiz To Html File

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
-let theQuiz=document.querySelector(".quiz")
-let result=document.querySelector(".result")
-let exam=document.querySelector(".exam")
+let contenerQuiz=document.querySelector(".contener-quiz")
 
-let answer=new XMLHttpRequest();
-answer.open("GET","quiz.json")
-answer.send()
+function serach(){
+    let inputQuiz=document.querySelector(".quiz").value.trim()
+    // let result=document.querySelector(".result")
+    let exam=document.querySelector(".exam")
+    let thequiz=document.querySelectorAll(".thequiz")
+    let theanser=document.querySelectorAll(".anser")
+for(let i=0;i<=thequiz.length;i++){
+    console.log(thequiz[i].textContent)
+    if(thequiz[i].innerHTML.trim().indexOf(inputQuiz)>=0){
+        thequiz[i].style.display=""
+        theanser[i].style.display=""
+    }else{
+        thequiz[i].style.display="none"
+        theanser[i].style.display="none"
 
-answer.onreadystatechange= function(){
-    if(this.status===200&&this.readyState===4){
-        let myAnswer=JSON.parse(this.responseText)
-        let corectOrFulse=myAnswer[ "صح او خطا"]
+    }
+}}
 
-        result.onclick=function(){
+// let answer=new XMLHttpRequest();
+// answer.open("GET","quiz.json")
+// answer.send()
 
-            for(let i = 0; i <= corectOrFulse.length; i++){
-                let allQuiz=corectOrFulse[i].quiz
+// answer.onreadystatechange= function(){
+//     if(this.status===200&&this.readyState===4){
+//         let myAnswer=JSON.parse(this.responseText)
+//         let corectOrFulse=myAnswer[ "صح او خطا"]
 
-                if(theQuiz.value.toString().trim()==allQuiz.toString().trim()){
-                    if(document.querySelector(".myResult")){
-                        document.querySelector(".myResult").remove()
-                    }
-                    let myResult=document.createElement("div")
-                    myResult.className="myResult"
-                    myResult.innerText=corectOrFulse[i].anser
-                    exam.appendChild(myResult)
-                }
-            
-        }
-    }}}
+        
+
+//             for(let i = 0; i <= corectOrFulse.length; i++){
+//                 let allQuiz=corectOrFulse[i].quiz
+//                 let arrarQuiz=Array.from(allQuiz)
+//             }}}
+
